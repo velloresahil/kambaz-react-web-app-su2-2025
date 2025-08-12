c716fb3be34e24c5102c91038fb7d46377e53e1f
velloresahil <velloresahil2002@gmail.com>
2025-08-12 22:40:48 +0530

    a6 second commit

--- src/Kambaz/Account/Users.tsx
+++ src/Kambaz/Account/Users.tsx
@@ -8,7 +8,7 @@ export default function Users() {
  const [users, setUsers] = useState<any[]>([]);
  const { uid } = useParams();
   const [role, setRole] = useState("");
-    const [name, setName] = useState("");
+    const [, setName] = useState("");
   const filterUsersByName = async (name: string) => {
     setName(name);
     if (name) {
